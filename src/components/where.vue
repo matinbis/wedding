b5476a3d79bb378d26c0a21648cea6f3624d6b8d
Matvey <matinbis@mail.ru>
2026-02-23 23:47:39 +0500

    currect

--- src/components/where.vue
+++ src/components/where.vue
@@ -11,7 +11,7 @@
         </div>
         <div class="party2">
             <p>Наш праздник будет проходить в банкетном зале <strong>“Мёд”</strong> по адресу  улица 50-летия Магнитки, 58</p>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.084279911328!2d58.95271597705008!3d53.35964657372784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43d12fed3f2d7275%3A0xd9109fb3ff536187!2z0JzQtdC0!5e0!3m2!1sru!2sru!4v1770740733051!5m2!1sru!2sru" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.084279911328!2d58.95271597705008!3d53.35964657372784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43d12fed3f2d7275%3A0xd9109fb3ff536187!2z0JzQtdC0!5e0!3m2!1sru!2sru!4v1770740733051!5m2!1sru!2sru" width="600" height="450" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
  </div>
